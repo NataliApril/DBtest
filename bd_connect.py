7fa56aa3a4922bbcbbbf78dcdf269ae16e59cdd8
Natali <natali.egorova4@yandex.ru>
2024-04-26 18:03:02 +0300

    data about DB

--- bd_connect.py
+++ bd_connect.py
@@ -1,12 +1,28 @@
+'''https://pythonru.com/biblioteki/vvedenie-v-postgresql-s-python-psycopg2'''
+
+
 import psycopg2
+import datetime
+from psycopg2 import Error
 
-conn = psycopg2.connect(dbname="tester", user="tester", password="postgres", host= "77.238.255.81", port="5432")
+try:
+    conn = psycopg2.connect(dbname="tester", user="tester", password="postgres", host= "77.238.255.81", port="5432")
 
-cur = conn.cursor()
+    # Курсор для выполнения операций с базой данных
+    cursor = conn.cursor()
+    # Распечатать сведения о PostgreSQL
+    print("Информация о сервере PostgreSQL")
+    print(conn.get_dsn_parameters(), "\n")
+    # Выполнение SQL-запроса
+    cursor.execute("SELECT version();")
+    # Получить результат
+    record = cursor.fetchone()
+    print("Вы подключены к - ", record, "\n")
 
-cur.execute('select * from people')
-
-results = cur.fetchall()
-
-for result in results:
-    print (result)
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
+finally:
+    if conn:
+        cursor.close()
+        conn.close()
+        print("Соединение с PostgreSQL закрыто")
